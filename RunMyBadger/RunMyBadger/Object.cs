5245d224ad36ac9a22eec76f1e5fbdcd19c41a63
SamuraiSusy <susyy-@hotmail.com>
2015-04-14 09:50:01 +0300

    xna
    project

---
+++ RunMyBadger/RunMyBadger/Object.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RunMyBadger
+{
+    public class Object
+    {
+        private Texture2D _texture;
+        //private Vector2 _position;
+
+        public Texture2D texture
+        {
+            get { return _texture; }
+            set { _texture = value; }
+        }
+    }
+}
