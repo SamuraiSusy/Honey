2ec65ff5440cb7eca4ad6615553ef1aa8bc58fce
Susanna Nurmi <susyy-@hotmail.com>
2015-05-13 11:21:52 +0300

    back to unity

---
+++ Honey/Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSettings : MonoBehaviour
+{
+    public float orthographicSize; // pitäis tulostaa kameran koko / 2
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    private void FixedUpdate()
+    {
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        Vector3 move = new Vector3(speed * Time.deltaTime, 0);
+        transform.Translate(move);
+    }
+}
---
+++ Honey/Assets/Scripts/PlayerContorl.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContorl : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public float raySize; // muuta pelaajan spriten koon mukaan
+    private bool isGrounded;
+
+	// Use this for initialization
+	private void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        DebugRaycast();
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float move = h * speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(move, 0));
+    }
+
+    private void Jump()
+    {
+        float v = Input.GetAxis("Jump");
+        float jump = v * jumpForce * Time.deltaTime;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, raySize);
+
+        if (hit != null)
+            isGrounded = true;
+        else
+            isGrounded = false;
+
+        if(isGrounded)
+            transform.Translate(new Vector3(0, jump));
+    }
+
+    private void DebugRaycast()
+    {
+        Debug.DrawRay(transform.position, -Vector3.up * raySize);
+        Debug.Log("is grounded " + isGrounded);
+    }
+}
